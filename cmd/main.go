c9e5ef59528f9588a1ddedf4de91541d5e4851df
ihciah <ihciah@gmail.com>
2020-03-11 12:43:34 +0800

    Add retry for telegram bot starting

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"time"
 
 	controller "github.com/ihciah/ipmi-controller"
 )
@@ -17,11 +18,19 @@ func main() {
 	}
 	ipmi := controller.NewIPMI(config)
 	go func() {
-		telegramBot, err := controller.NewTelegramBot(ipmi)
-		if err != nil {
-			log.Printf("can not run telegram bot: %v", err)
-		} else {
-			telegramBot.Serve()
+		for {
+			telegramBot, err := controller.NewTelegramBot(ipmi)
+			if err != nil {
+				log.Printf("can not run telegram bot: %v", err)
+				if ipmi.TelegramConfig.Token == "" {
+					log.Print("telegram is not enabled")
+					return
+				}
+				log.Print("will sleep 10 seconds and retry starting telegram bot")
+				time.Sleep(10 * time.Second)
+			} else {
+				telegramBot.Serve()
+			}
 		}
 	}()
 	controller.ControlFanSpeed(ipmi)
